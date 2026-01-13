52795308a07bc8f0bdfcafabc5980c5b83fcaf0c
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2026-01-12 22:48:16 -0300

    WIP: Criado o componente de badge

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Text } from "./components/text";
 import TrashIcon from "./assets/icons/trash.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
 import { Icon } from "./components/icon";
+import { Badge } from "./components/badge";
 
 export default function App() {
   return (
@@ -9,6 +10,8 @@ export default function App() {
       <Text variant={"body-md"}>Olá Mundo</Text>
       <Icon svg={TrashIcon} />
       <Icon svg={SpinnerIcon} animate />
+      <Badge variant="secondary">5</Badge>
+      <Badge>2 de 5</Badge>
     </>
   );
 }
