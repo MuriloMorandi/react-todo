b51021e8a78c30194e0f8f70f393cba85914bbce
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2026-01-12 22:27:36 -0300

    WIP: Criado o componente de icones

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Text } from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import { Icon } from "./components/icon";
 
 export default function App() {
   return (
     <>
       <Text variant={"body-md"}>Olá Mundo</Text>
+      <Icon svg={TrashIcon} />
+      <Icon svg={SpinnerIcon} animate />
     </>
   );
 }
